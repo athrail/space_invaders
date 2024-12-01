12afb1db31be2ed2d41556cec8ec6d29062d2ae0
Krzysztof Lewandowski <krzlew1@gmail.com>
2024-12-01 23:37:59 +0100

    shooting, score rendering

--- main.c
+++ main.c
@@ -1,23 +1,28 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 #define ARR_LEN(a) (sizeof(a)/sizeof(a[0]))
 #define DEBUG(format, ...) printf("%s:%d:%s() " format "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);
+#define FRAME_SLEEP 16
 #define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_HEIGHT 800
+#define BULLET_COUNT 1
 
-#define ENEMY_WIDTH 40
+#define ENEMY_WIDTH 30
 #define ENEMY_HEIGHT 20
-#define ENEMY_SPACING 20
-#define ENEMY_PER_LINE 12
+#define ENEMY_PER_LINE 10
 
-#define BARRIER_COUNT 3
-#define BARRIER_WIDTH 100
-#define BARRIER_HEIGHT 40
+#define BARRIER_COUNT 4
+#define BARRIER_WIDTH 70
+#define BARRIER_HEIGHT 50
+
+#define PLAYER_WIDTH 40
+#define PLAYER_HEIGHT 30
+#define SHOOT_DELAY 3000
 
 typedef struct {
   SDL_Rect rect;
@@ -25,10 +30,25 @@ typedef struct {
 
 typedef struct {
   SDL_Rect rect;
+  int shooting_timer;
 } Player_t;
 
 typedef struct {
   SDL_Rect rect;
+  int velocity;
+  bool active;
+} Bullet_t;
+
+typedef enum {
+  New,
+  Small_Damage,
+  Hard_Damage,
+  Destroyed
+} Barrier_Level_t;
+
+typedef struct {
+  SDL_Rect rect;
+  Barrier_Level_t level;
 } Barrier_t;
 
 typedef struct {
@@ -38,11 +58,21 @@ typedef struct {
   size_t       enemy_count;
   Player_t     *player;
   Barrier_t    *barriers;
+  TTF_Font     *sans;
+  unsigned int score;
+  SDL_Texture  *score_texture;
+  Bullet_t     *bullets;
+  unsigned int bullet_id;
 } App_t;
 
 void init_sdl(App_t *app) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+    exit(1);
+  }
+
+  if (TTF_Init() < 0) {
+    printf("Couldn't initialize SDL_TTF: %s\n", SDL_GetError());
     exit(1);
   }
 
@@ -57,16 +87,79 @@ void init_sdl(App_t *app) {
     printf("Couldn't create renderer: %s\n", SDL_GetError());
     exit(1);
   }
+
+  app->sans = TTF_OpenFont("arcade.ttf", 24);
+  if (app->sans == NULL) {
+    printf("Couldn't load arcade.ttf font: %s\n", SDL_GetError());
+    exit(1);
+  }
 }
 
-void handle_input(void) {
+void update_score(App_t *app, int change) {
+  app->score += change;
+  char *buf = malloc(sizeof(char) * 255);
+  memset(buf, 0, sizeof(char) * 255);
+  sprintf(buf, "Score   %08d", app->score);
+  SDL_Surface *score_surface = TTF_RenderText_Solid(app->sans, buf, (SDL_Color){255, 255, 255});
+  app->score_texture = SDL_CreateTextureFromSurface(app->renderer, score_surface);
+}
+
+void spawn_bullet(App_t *app, SDL_Point loc, int velocity) {
+  DEBUG("spawn_bullet with id: %d", app->bullet_id);
+  app->bullets[app->bullet_id].active = true;
+  app->bullets[app->bullet_id].rect = (SDL_Rect){loc.x, loc.y, 5, 10};
+  app->bullets[app->bullet_id].velocity = velocity;
+  app->bullet_id = (app->bullet_id + 1) % BULLET_COUNT;
+
+  DEBUG("bullet_id after spawn: %d", app->bullet_id);
+}
+
+void handle_input(App_t *app) {
   SDL_Event event = {0};
 
   while (SDL_PollEvent(&event)) {
     switch(event.type) {
       case SDL_QUIT:
-      exit(0);
-      break;
+        exit(0);
+        break;
+
+      case SDL_KEYDOWN:
+        switch(event.key.keysym.sym) {
+          case SDLK_LEFT:
+            app->player->rect.x = app->player->rect.x > 0 ? app->player->rect.x - 10 : 0;
+            break;
+          case SDLK_RIGHT:
+            app->player->rect.x = app->player->rect.x < WINDOW_WIDTH - PLAYER_WIDTH ? app->player->rect.x + 10 : WINDOW_WIDTH - PLAYER_WIDTH;
+            break;
+          case SDLK_SPACE:
+            if (app->player->shooting_timer <= 0) {
+              DEBUG("Pressed shot");
+              app->player->shooting_timer = SHOOT_DELAY;
+              SDL_Point loc = {app->player->rect.x + (app->player->rect.w - 5) / 2, app->player->rect.y - 10};
+              spawn_bullet(app, loc, -5);
+            }
+            update_score(app, 100);
+          default:
+            break;
+        }
+        break;
+    }
+  }
+}
+
+void update(App_t *app) {
+  handle_input(app);
+
+  if (app->player->shooting_timer) {
+    app->player->shooting_timer -= FRAME_SLEEP;
+  }
+
+  for (size_t i = 0; i < BULLET_COUNT; i++) {
+    if (app->bullets[i].active) {
+      app->bullets[i].rect.y += app->bullets[i].velocity;
+      if ((app->bullets[i].rect.y > WINDOW_HEIGHT) || (app->bullets[i].rect.y < 0)) {
+        app->bullets[i].active = false;
+      }
     }
   }
 }
@@ -85,10 +178,32 @@ void render(App_t *app) {
   }
 
   // Player rendering
+  SDL_SetRenderDrawColor(app->renderer, 255, 0, 0, 255);
+  SDL_RenderFillRect(app->renderer, &app->player->rect);
+
+  SDL_SetRenderDrawColor(app->renderer, 255, 255, 255, 255);
+  for (size_t i = 0; i < BULLET_COUNT; i++) {
+    if (app->bullets[i].active) {
+      SDL_RenderFillRect(app->renderer, &app->bullets[i].rect);
+    }
+  }
+
+  SDL_Rect score_rect = {0};
+  SDL_QueryTexture(app->score_texture, NULL, NULL, &score_rect.w, &score_rect.h);
+  SDL_RenderCopy(app->renderer, app->score_texture, NULL, &score_rect);
 }
 
 void init_player(App_t *app) {
+  app->player = malloc(sizeof(Player_t));
 
+  app->player->rect = (SDL_Rect){
+    (WINDOW_WIDTH - PLAYER_WIDTH) / 2,
+    WINDOW_HEIGHT - 10 - PLAYER_HEIGHT,
+    PLAYER_WIDTH,
+    PLAYER_HEIGHT
+  };
+
+  update_score(app, 0);
 }
 
 void init_barriers(App_t *app) {
@@ -98,10 +213,11 @@ void init_barriers(App_t *app) {
     exit(1);
   }
 
+  int spacing = (WINDOW_WIDTH - (BARRIER_COUNT * BARRIER_WIDTH)) / (BARRIER_COUNT + 1);
   for(size_t i = 0; i < BARRIER_COUNT; i++) {
     app->barriers[i].rect = (SDL_Rect){
-      100 + (i * 250),
-      475,
+      (i + 1) * spacing + i * BARRIER_WIDTH,
+      500,
       BARRIER_WIDTH,
       BARRIER_HEIGHT
     };
@@ -118,15 +234,29 @@ void init_enemies(App_t *app, unsigned int count) {
   }
 
   // Add maybe some static initial offset to center enemies on screen
+  int spacing = (WINDOW_WIDTH - 100 - (ENEMY_PER_LINE * ENEMY_WIDTH)) / (ENEMY_PER_LINE + 1);
+
   app->enemy_count = count;
   DEBUG("set app->enemy_count to %zu", app->enemy_count);
   for(size_t i = 0; i < count; i++) {
     app->enemies[i].rect = (SDL_Rect){
-      ((i % ENEMY_PER_LINE) + 1) * ENEMY_SPACING + (i % ENEMY_PER_LINE) * ENEMY_WIDTH,
-      ((i / ENEMY_PER_LINE) + 1) * ENEMY_SPACING + (i / ENEMY_PER_LINE) * ENEMY_HEIGHT,
+      ((i % ENEMY_PER_LINE) + 1) * spacing + (i % ENEMY_PER_LINE) * ENEMY_WIDTH,
+      20 + ((i / ENEMY_PER_LINE) + 1) * spacing + (i / ENEMY_PER_LINE) * ENEMY_HEIGHT,
       ENEMY_WIDTH,
       ENEMY_HEIGHT
     };
+  }
+}
+
+void init_app(App_t *app) {
+  init_enemies(app, ENEMY_PER_LINE * 5);
+  init_barriers(app);
+  init_player(app);
+
+  app->bullets = calloc(50, sizeof(Bullet_t));
+  if (app->bullets == NULL) {
+    printf("Couldn't allocate memory for bullets...\n");
+    exit(1);
   }
 }
 
@@ -134,19 +264,17 @@ int main() {
   App_t app = {0};
 
   init_sdl(&app);
-  init_enemies(&app, ENEMY_PER_LINE * 5);
-  init_barriers(&app);
-  init_player(&app);
+  init_app(&app);
 
   while(1) {
     SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 255);
     SDL_RenderClear(app.renderer);
 
-    handle_input();
+    update(&app);
     render(&app);
 
     SDL_RenderPresent(app.renderer);
-    SDL_Delay(16);
+    SDL_Delay(FRAME_SLEEP);
   }
 
   return 0;
