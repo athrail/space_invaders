d430ec4168a66c29159e8669906985c8b3802106
Krzysztof Lewandowski <krzlew1@gmail.com>
2024-12-11 21:56:18 +0100

    Sort out the dependency and install raylib on system after build

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@
 # set -xe
 
 CFLAGS="-Wall -Wextra -ggdb"
-CINCLUDES="-I/home/athrail/probe/raylib/raylib-5.5_linux_amd64/include"
-CLIBPATHS="-L/home/athrail/probe/raylib/raylib-5.5_linux_amd64/lib"
 CLIBS="-lraylib -lm"
 
-export LD_LIBRARY_PATH="/home/athrail/probe/raylib/raylib-5.5_linux_amd64/lib:$LD_LIBRARY_PATH"
-
-clang $CFLAGS -o build/space_invaders main.c player.c enemy.c $CLIBS $CINCLUDES $CLIBPATHS
+clang $CFLAGS -o build/space_invaders main.c player.c enemy.c $CLIBS #$CINCLUDES $CLIBPATHS
